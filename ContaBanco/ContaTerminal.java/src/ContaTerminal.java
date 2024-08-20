46a6ea779125b072591ec0a95a5319a76c408684
Julio Neto <julioramos3@gmail.com>
2024-08-20 15:23:13 -0300

    Inicial commit projeto basico java dio -claroboot

---
+++ ContaBanco/ContaTerminal.java/src/ContaTerminal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+      //TODO: importar a classe Scanner para coleta de dados via terminal.
+      //Feito.
+      // TODO: inicializar variáveis e coletar dados
+
+      
+
+    }
+}
